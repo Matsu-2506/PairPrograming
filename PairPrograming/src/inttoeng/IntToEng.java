58108be12fc5b0bf98e9ac3e21baa753ee902b23
aikosuzuki <aikosuzuki@192.168.3.10>
2020-06-12 12:49:29 +0900

    3rd switchに変更、0-9の出力

--- PairPrograming/src/inttoeng/IntToEng.java
+++ PairPrograming/src/inttoeng/IntToEng.java
@@ -5,52 +5,57 @@ import java.text.DecimalFormat;
 
 public class IntToEng {
     // メインメソッド
-   
-    private static final String[] tensNames = {
-    	    "",
-    	    " ten",
-    	    " twenty",
-    	    " thirty",
-    	    " forty",
-    	    " fifty",
-    	    " sixty",
-    	    " seventy",
-    	    " eighty",
-    	    " ninety"
-    };
-    private static final String[] numNames = {
-    	    "",
-    	    " one",
-    	    " two",
-    	    " three",
-    	    " four",
-    	    " five",
-    	    " six",
-    	    " seven",
-    	    " eight",
-    	    " nine",
-    	    " ten",
-    	    " eleven",
-    	    " twelve",
-    	    " thirteen",
-    	    " fourteen",
-    	    " fifteen",
-    	    " sixteen",
-    	    " seventeen",
-    	    " eighteen",
-    	    " nineteen"
-    };
+	public String print1 (int a) {
+		String no = "";
+		switch (a) {
+			case 0: no = "zero"; break;
+			case 1: no = "one"; break;
+			case 2: no = "two"; break;
+			case 3: no = "three"; break;
+			case 4: no = "four"; break;
+			case 5: no = "five"; break;
+			case 6: no = "six"; break;
+			case 7: no = "seven"; break;
+			case 8: no = "eight"; break;
+			case 9: no = "nine"; break;
+		}
+		return no;
+	}
+	public String printTeen (int a) {
+		String no = "";
+		switch (a%10) {
+		case 0: no = "ten"; break;
+		case 1: no = "eleven"; break;
+		case 2: no = "twelve"; break;
+		case 3: no = "thirteen"; break;
+		case 4: no = "fourteen"; break;
+		case 5: no = "fifteen"; break;
+		case 6: no = "sixteen"; break;
+		case 7: no = "seventeen"; break;
+		case 8: no = "eighteen"; break;
+		case 9: no = "nineteen"; break;
+		}
+		return no;
+	}
+	
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int input = sc.nextInt();
+        IntToEng ite = new IntToEng();
+        if(0<input && input<10) {
+        	System.out.print(ite.printTeen(input));
+        }
         //System.out.println(translateEng(input));
     }
 
-    // 数値を英訳する変換するメソッド
-    /*static String translateEng(int n) {
+    
+    static String translateEng(int n) {
+    	
+    	
         return "";
     }
-    */
+    
 }		
 
 
